98434d8bfaf37ded22ca56894ff6a89b2688ca5b
Hans Thummel <enikath@gmail.com>
2019-07-11 14:25:03 -0500

    stuff

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-#shirt {
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -20;
-}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -314,4 +314,18 @@ canvas {
 input {
   text-align: center;
 }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#shirt {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -20;
+}
 </style>
